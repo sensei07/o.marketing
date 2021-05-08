a804abb6f0d5b20d8d339d0f0d59fe96190d7bd6
Andrii Tomkiv <51452880+sensei07@users.noreply.github.com>
2021-05-08 18:31:16 +0300

    ending all

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-$(function(){
-  var mixer = mixitup('.portfolio__content');
+$(function () {
+  $(".menu a, .go-top").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="left arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="right arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $(".menu__btn, .menu a").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
+
+  let mixer = mixitup(".portfolio__content");
 });
